cb5915bddce6a8a6668e0c032a630db2c0894b23
Cabelow <rafaelmello.cabelow@gmail.com>
2021-07-04 21:09:02 -0300

    json, class e paht

---
+++ Portifolio.js
@@ -0,0 +1,8 @@
+class Portifolio {
+    constructor(foto, descricao){
+        this.foto = foto
+        this.descricao = descricao
+    }
+}
+
+module.exports = Portifolio
---
+++ index.js
@@ -0,0 +1,32 @@
+var HTTP = require('http');
+var URL = require('url');
+const Portifolio = require('./Portifolio')
+
+var controller = function (req, res) {
+
+    const parts = URL.parse(req.url)
+    const path = parts.path
+
+    if (path == '/') {
+        res.end("Rafael Mello")
+    }
+    else if (path == '/bio') {
+        res.end("Biografia")
+    }
+    else if (path == '/portifolio') {
+        let portifolio = []
+        portifolio.push(new Portifolio("perfil", "foto do perfil"))
+        portifolio.push(new Portifolio("fundo", "foto do fundo"))
+        res.end(JSON.stringify(portifolio))
+    }
+    else {
+        res.end("erro 404")
+    }
+
+    res.writeHead(200, { "Content-Type": "text/plain" });
+};
+
+var server = HTTP.createServer(controller);
+
+server.listen(3000);
+console.log("Servidor iniciado em http://localhost:3000/");
